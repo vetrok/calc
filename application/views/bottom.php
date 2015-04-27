17e861e63b76ab97ba7b2c2938ff4a5161034846
vetrok <vetrok87@gmail.com>
2015-04-27 21:32:50 +0300

    Style changed

--- application/views/bottom.php
+++ application/views/bottom.php
@@ -1,6 +1,12 @@
 		</div>
         </div>
 	</div>
-	<div class='bottom'><span>Powered By: <?php echo $_SERVER["SERVER_SOFTWARE"];?></span></div>
+        <div class="hiddenB"></div>
+        <div class='bottom'>
+            <br>
+            <span>
+                Powered By: <?php echo $_SERVER["SERVER_SOFTWARE"];?>
+            </span>
+        </div>
 </body>
 </html>
--- application/views/middleSeansIndex.php
+++ application/views/middleSeansIndex.php
@@ -2,7 +2,7 @@
     echo "<div class='header'>Калькулятор Сеансов</div>\n";
 ?>
 <div class="msg">
-    На данный момент раздел находится на Реконструкции.
+    На данный момент раздел находится в разработке.
 </div>
 <!--form action='' method='GET'>
 	<input type='text' name='name'><br>
